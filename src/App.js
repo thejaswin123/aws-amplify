39e633f0169e4c9039f508e3837a6f80b747f5ec
Thejaswin <thejas2002@yahoo.in>
2021-12-03 19:49:11 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+
+
+Amplify.configure(awsconfig);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                
+                <>
+                <canvas id="input-canvas"></canvas>
+                <canvas id="overlay-canvas"></canvas>
+                <video id="webcam-video" preload="auto" loop playsinline autoplay></video>
+                <script src="./dlib/dlib-wasm-test.js"></script>
+                <script src="./dlib/dlib-wasm-test-js.js"></script>
+                </>
+                <AmplifySignOut />
+            </header>
+        </div>
+    );
 }
 
-export default App;
+export default withAuthenticator(App);
